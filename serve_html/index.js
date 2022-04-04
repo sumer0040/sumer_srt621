f57d1b55ea7442bd765fa619a1ffe06cbcd4f3d3
sumer0040 <101667544+sumer0040@users.noreply.github.com>
2022-04-03 21:06:19 -0400

    Delete serve_html directory

--- serve_html/index.js
+++
@@ -1,6 +0,0 @@
-const port=3000;
-express = require('express');
-app = express();
-routing = require(__dirname+'/express.js');
-app.use('/', routing);
-app.listen(port, ()=> {console.log(`Server Started on Port: ${port}`);});
