e64421b09cd63dbfa4293686b17eb4845c7bd608
sumer0040 <101667544+sumer0040@users.noreply.github.com>
2022-04-03 21:02:58 -0400

    Delete serve_html directory

--- serve_html/express.js
+++
@@ -1,6 +0,0 @@
-var path=require('path');
-var express=require('express');
-var app=express();
-app.use(express.static(path.join(__dirname+'/Public')))
-app.get('/home',(req,res) =>{res.sendFile(path.join(__dirname+'/Views/home.html'));});
-module.exports = app; 
